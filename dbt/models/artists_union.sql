02a0211459fb73a5591b04b9b75f1a1e0c302ead
Olivier Gozlan <olivier.gozlan75@gmail.com>
2025-09-29 10:47:10 +0000

    modification requete DBT

--- dbt/models/artists_union.sql
+++ dbt/models/artists_union.sql
@@ -1,6 +1,6 @@
 with deezer as (
     select *
-    from {{ source('music_dataset', 'deezer_results') }}
+    from {{ source('music_dataset', 'deezer_artists') }}
 ),
 spotify as (
     select *
@@ -14,10 +14,10 @@ select
     s.spotify_artist_url,
     s.spotify_artist_total_followers,
     s.spotify_artist_popularity,
-    d.link as deezer_link,
-    d.name as deezer_name,
-    d.nb_fan as deezer_nb_fan,
+    d.deezer_artist_url,
+    d.deezer_artist_name,
+    d.deezer_artist_total_followers,
     current_date() as ingestion_date
 from spotify s
 left join deezer d
-on s.spotify_artist_name = d.name
+on s.spotify_artist_name = d.deezer_artist_name
